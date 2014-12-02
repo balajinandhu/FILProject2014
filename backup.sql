39dce2103cec0f3506a9252cf26f2bd0dbaae81e
kslsruti <skoduri@scu.edu>
2014-12-01 16:33:09 -0800

    please use backup.sql for importing updated tables

--- backup.sql
+++ backup.sql
@@ -16,6 +16,39 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `conversation_details`
+--
+
+DROP TABLE IF EXISTS `conversation_details`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `conversation_details` (
+  `conversation_ID` int(11) NOT NULL DEFAULT '0',
+  `team_name` varchar(100) DEFAULT NULL,
+  `subject` varchar(255) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  `date_of_conversation` date DEFAULT NULL,
+  `project_ID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`conversation_ID`),
+  KEY `team_name` (`team_name`),
+  KEY `project_ID` (`project_ID`),
+  KEY `user_name` (`user_name`),
+  CONSTRAINT `conversation_details_ibfk_1` FOREIGN KEY (`team_name`) REFERENCES `teaminfo` (`team_name`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `conversation_details_ibfk_2` FOREIGN KEY (`project_ID`) REFERENCES `projectinfo` (`project_ID`) ON DELETE SET NULL ON UPDATE CASCADE,
+  CONSTRAINT `conversation_details_ibfk_3` FOREIGN KEY (`user_name`) REFERENCES `userrecord` (`user_name`) ON DELETE SET NULL ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `conversation_details`
+--
+
+LOCK TABLES `conversation_details` WRITE;
+/*!40000 ALTER TABLE `conversation_details` DISABLE KEYS */;
+/*!40000 ALTER TABLE `conversation_details` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `projectinfo`
 --
 
@@ -24,7 +57,6 @@ DROP TABLE IF EXISTS `projectinfo`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `projectinfo` (
   `project_ID` int(11) NOT NULL DEFAULT '0',
-  `team_ID` int(11) DEFAULT NULL,
   `project_name` varchar(50) DEFAULT NULL,
   `project_description` varchar(100) DEFAULT NULL,
   `status` varchar(50) DEFAULT NULL,
@@ -32,9 +64,12 @@ CREATE TABLE `projectinfo` (
   `start_date` date DEFAULT NULL,
   `end_date` date DEFAULT NULL,
   `estimated_budget` int(11) DEFAULT NULL,
+  `team_name` varchar(50) DEFAULT NULL,
+  `outcome` varchar(150) DEFAULT NULL,
   PRIMARY KEY (`project_ID`),
-  KEY `team_ID` (`team_ID`),
-  CONSTRAINT `projectinfo_ibfk_1` FOREIGN KEY (`team_ID`) REFERENCES `teaminfo` (`team_ID`)
+  KEY `team_name` (`team_name`),
+  CONSTRAINT `projectinfo_ibfk_1` FOREIGN KEY (`team_name`) REFERENCES `teaminfo` (`team_name`) ON DELETE SET NULL,
+  CONSTRAINT `projectinfo_ibfk_2` FOREIGN KEY (`team_name`) REFERENCES `teaminfo` (`team_name`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -44,8 +79,66 @@ CREATE TABLE `projectinfo` (
 
 LOCK TABLES `projectinfo` WRITE;
 /*!40000 ALTER TABLE `projectinfo` DISABLE KEYS */;
-INSERT INTO `projectinfo` VALUES (210,11,'ooad','fil','processing','good','2014-12-22','2014-12-30',100);
+INSERT INTO `projectinfo` VALUES (210,'ooad','fil','processing','good','2014-12-22','2014-12-30',100,NULL,NULL),(999,'ooad1','water','good','yes','2013-12-12','2015-12-12',2000,NULL,NULL);
 /*!40000 ALTER TABLE `projectinfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `projectmetriclkp`
+--
+
+DROP TABLE IF EXISTS `projectmetriclkp`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `projectmetriclkp` (
+  `project_metric_ID` int(11) NOT NULL DEFAULT '0',
+  `project_metric_desc` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`project_metric_ID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `projectmetriclkp`
+--
+
+LOCK TABLES `projectmetriclkp` WRITE;
+/*!40000 ALTER TABLE `projectmetriclkp` DISABLE KEYS */;
+/*!40000 ALTER TABLE `projectmetriclkp` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `taskinfo`
+--
+
+DROP TABLE IF EXISTS `taskinfo`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `taskinfo` (
+  `task_ID` int(11) NOT NULL DEFAULT '0',
+  `project_ID` int(11) DEFAULT NULL,
+  `start_date` date DEFAULT NULL,
+  `end_date` date DEFAULT NULL,
+  `outcome` varchar(50) DEFAULT NULL,
+  `project_metric_ID` int(11) DEFAULT NULL,
+  `project_metric_value` int(11) DEFAULT NULL,
+  `user_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`task_ID`),
+  KEY `project_ID` (`project_ID`),
+  KEY `user_name` (`user_name`),
+  CONSTRAINT `taskinfo_ibfk_1` FOREIGN KEY (`project_ID`) REFERENCES `projectinfo` (`project_ID`),
+  CONSTRAINT `taskinfo_ibfk_2` FOREIGN KEY (`user_name`) REFERENCES `userrecord` (`user_name`) ON DELETE SET NULL,
+  CONSTRAINT `taskinfo_ibfk_3` FOREIGN KEY (`user_name`) REFERENCES `userrecord` (`user_name`) ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `taskinfo`
+--
+
+LOCK TABLES `taskinfo` WRITE;
+/*!40000 ALTER TABLE `taskinfo` DISABLE KEYS */;
+INSERT INTO `taskinfo` VALUES (1,210,'2014-12-01','2014-12-12','class diagram',5,78,NULL);
+/*!40000 ALTER TABLE `taskinfo` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -56,9 +149,9 @@ DROP TABLE IF EXISTS `teaminfo`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `teaminfo` (
-  `team_ID` int(11) NOT NULL DEFAULT '0',
-  `team_name` varchar(100) DEFAULT NULL,
-  PRIMARY KEY (`team_ID`)
+  `team_name` varchar(100) NOT NULL DEFAULT '',
+  `team_strength` int(11) DEFAULT NULL,
+  PRIMARY KEY (`team_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -68,8 +161,59 @@ CREATE TABLE `teaminfo` (
 
 LOCK TABLES `teaminfo` WRITE;
 /*!40000 ALTER TABLE `teaminfo` DISABLE KEYS */;
-INSERT INTO `teaminfo` VALUES (10,'sruti'),(11,'abc'),(20,'vamshi');
+INSERT INTO `teaminfo` VALUES ('abc',NULL),('amar',NULL),('cim',NULL),('sruti',NULL),('vamshi',NULL);
 /*!40000 ALTER TABLE `teaminfo` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tempaddmember`
+--
+
+DROP TABLE IF EXISTS `tempaddmember`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tempaddmember` (
+  `user_name` varchar(100) NOT NULL DEFAULT '',
+  `role` varchar(50) DEFAULT NULL,
+  `team_name` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`user_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tempaddmember`
+--
+
+LOCK TABLES `tempaddmember` WRITE;
+/*!40000 ALTER TABLE `tempaddmember` DISABLE KEYS */;
+INSERT INTO `tempaddmember` VALUES ('sai','manager','cim'),('satya','prof','aaaa');
+/*!40000 ALTER TABLE `tempaddmember` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tempaddteam`
+--
+
+DROP TABLE IF EXISTS `tempaddteam`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tempaddteam` (
+  `team_name` varchar(50) NOT NULL DEFAULT '',
+  `team_member1` varchar(100) DEFAULT NULL,
+  `team_member2` varchar(100) DEFAULT NULL,
+  `team_member3` varchar(100) DEFAULT NULL,
+  `team_member4` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`team_name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tempaddteam`
+--
+
+LOCK TABLES `tempaddteam` WRITE;
+/*!40000 ALTER TABLE `tempaddteam` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tempaddteam` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -80,16 +224,16 @@ DROP TABLE IF EXISTS `userrecord`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `userrecord` (
-  `user_ID` int(11) NOT NULL DEFAULT '0',
-  `user_name` varchar(100) DEFAULT NULL,
+  `user_name` varchar(100) NOT NULL DEFAULT '',
   `password` varchar(20) DEFAULT NULL,
   `role` varchar(50) DEFAULT NULL,
-  `team_ID` int(11) DEFAULT NULL,
   `email_ID` varchar(50) DEFAULT NULL,
   `phone_number` int(11) DEFAULT NULL,
-  PRIMARY KEY (`user_ID`),
-  KEY `team_ID` (`team_ID`),
-  CONSTRAINT `userrecord_ibfk_1` FOREIGN KEY (`team_ID`) REFERENCES `teaminfo` (`team_ID`)
+  `team_name` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`user_name`),
+  KEY `team_name` (`team_name`),
+  CONSTRAINT `userrecord_ibfk_1` FOREIGN KEY (`team_name`) REFERENCES `teaminfo` (`team_name`) ON DELETE SET NULL,
+  CONSTRAINT `userrecord_ibfk_2` FOREIGN KEY (`team_name`) REFERENCES `teaminfo` (`team_name`) ON UPDATE CASCADE
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -99,7 +243,7 @@ CREATE TABLE `userrecord` (
 
 LOCK TABLES `userrecord` WRITE;
 /*!40000 ALTER TABLE `userrecord` DISABLE KEYS */;
-INSERT INTO `userrecord` VALUES (1,'vam','1233','prof',20,'vam@gamail',4268),(29,'sru','123koi','student',11,'soduri@scu.edu',315),(209,'srutiksl','123koduri','student',10,'skoduri@scu.edu',314);
+INSERT INTO `userrecord` VALUES ('clara','12345','stu','gmail',98,NULL),('college','456ft','stu','try@gmail.com',5678,NULL),('engg','456ft','stu','try@gmail.com',5678,NULL),('santa','12345','stu','gmail',98,NULL),('sru','123koi','student','skoduri@scu.edu',315,NULL),('sruti','123koi','student','soduri@scu.edu',315,NULL),('srutiksl','123koduri','student','skoduri@scu.edu',314,NULL),('stanley','456ft','stu','try@gmail.com',5678,NULL),('tech','456ft','stu','try@gmail.com',5678,NULL),('university','12345','stu','gmail',98,NULL),('vam','1233','prof','vam@gamail',4268,NULL);
 /*!40000 ALTER TABLE `userrecord` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -112,4 +256,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-11-29 17:11:30
+-- Dump completed on 2014-12-01 16:16:28
